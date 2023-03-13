31a954af5219fc9583ac0ae83aa1fba72c4b9068
Churino10 <davidlopezporven1@gmail.com>
2023-03-13 10:34:15 +0100

    inicio

---
+++ proyecto/gestionfp/src/test/java/es/aulanosa/gestionfp/GestionfpApplicationTests.java
@@ -0,0 +1,13 @@
+package es.aulanosa.gestionfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
