577aa67b488f16d13caf2b609a9309090a8dadc0
Churino10 <davidlopezporven1@gmail.com>
2023-03-14 10:54:37 +0100

    un poco mas

---
+++ proyecto/gestionfp/src/test/java/es/aulanosa/gestionfp/TestAsignatura.java
@@ -0,0 +1,22 @@
+package es.aulanosa.gestionfp;
+
+import es.aulanosa.gestionfp.model.Asignatura;
+import es.aulanosa.gestionfp.service.AsignaturaService;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class TestAsignatura {
+
+    @Autowired
+    private AsignaturaService asignaturaService;
+
+
+
+}
