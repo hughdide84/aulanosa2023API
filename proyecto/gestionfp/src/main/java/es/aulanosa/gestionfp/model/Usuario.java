14b622032ea6c685b332991bf4737cacb381f29c
Churino10 <davidlopezporven1@gmail.com>
2023-03-14 12:04:09 +0100

    test terminados

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Usuario.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Usuario.java
@@ -19,18 +19,22 @@ public class Usuario {
     @Column(name = "nombre")
     @NotBlank(message = "El nombre no puede estar vacío")
     @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
+    @NotNull(message = "El nombre no puede ser nulo")
     private String nombre;
     @Column(name = "password")
     @NotBlank(message = "La contraseña no puede estar vacía")
     @Size(max = 100, message = "La contraseña no puede tener más de 100 caracteres")
+    @NotNull(message = "La contraseña no puede ser nula")
     private String password;
     @Column(name = "email")
     @NotBlank(message = "El email no puede estar vacío")
     @Size(max = 50, message = "El email no puede tener más de 50 caracteres")
+    @NotNull(message = "El email no puede ser nulo")
     private String email;
     @Column(name = "rol")
     @NotBlank(message = "El rol no puede estar vacío")
     @Size(max = 10, message = "El rol no puede tener más de 10 caracteres")
+    @NotNull(message = "El rol no puede ser nulo")
     private String rol;
 
     public Usuario(String nombre, String password, String email, String rol) {
