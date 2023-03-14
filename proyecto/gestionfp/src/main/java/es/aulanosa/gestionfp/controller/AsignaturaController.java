57e0c21e208b43d1248d6748f546ec2fb2c1c80b
Churino10 <davidlopezporven1@gmail.com>
2023-03-14 13:22:03 +0100

    controller y dto

---
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/AsignaturaController.java
@@ -0,0 +1,75 @@
+package es.aulanosa.gestionfp.controller;
+
+import es.aulanosa.gestionfp.dto.AsignaturaDTO;
+import es.aulanosa.gestionfp.model.Asignatura;
+import es.aulanosa.gestionfp.service.AsignaturaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/asignaturas")
+public class AsignaturaController {
+
+    @Autowired
+    private AsignaturaService asignaturaService;
+
+    @PostMapping
+    public ResponseEntity<?> insertarAsignatura(@RequestBody AsignaturaDTO asignaturaDTO) {
+
+        Asignatura asignaturaConsultada = asignaturaService.findById(asignaturaDTO.getId());
+
+        if (asignaturaConsultada == null) {
+            asignaturaService.save(asignaturaDTO.toAsignatura());
+            return ResponseEntity.status(HttpStatus.CREATED).body(asignaturaDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ya existe una asignatura con ese id");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAsignaturaById(@PathVariable int id) {
+        Asignatura asignatura = asignaturaService.findById(id);
+        if (asignatura != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(asignatura);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado la asignatura");
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllAsignaturas() {
+        List<Asignatura> asignaturas = asignaturaService.findAll();
+        if (asignaturas != null && !asignaturas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(asignaturas);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se han encontrado asignaturas");
+        }
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<?> updateAsignatura(@RequestBody AsignaturaDTO asignaturaDTO) {
+        Asignatura asignaturaConsultada = asignaturaService.findById(asignaturaDTO.getId());
+
+        if (asignaturaConsultada != null) {
+            asignaturaService.update(asignaturaDTO.toAsignatura());
+            return ResponseEntity.status(HttpStatus.OK).body(asignaturaDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado la asignatura");
+        }
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<?> borrarAsignatura(@PathVariable int id) {
+        Asignatura asignaturaConsultada = asignaturaService.findById(id);
+        if (asignaturaConsultada != null) {
+            asignaturaService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado la asignatura");
+        }
+    }
+}
--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/AsignaturaDTO.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/AsignaturaDTO.java
@@ -1,5 +1,38 @@
 package es.aulanosa.gestionfp.dto;
 
+import es.aulanosa.gestionfp.model.Asignatura;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AsignaturaDTO {
-    
+
+    private int id;
+    private String nombre;
+    private int idCurso;
+    private int idEstudios;
+    private int nivel;
+
+    public AsignaturaDTO(String nombre, int idCurso, int idEstudios, int nivel) {
+        this.nombre = nombre;
+        this.idCurso = idCurso;
+        this.idEstudios = idEstudios;
+        this.nivel = nivel;
+    }
+
+
+    public Asignatura toAsignatura(){
+        return new Asignatura(this.id, this.idCurso, this.idEstudios, this.nombre,  this.nivel);
+    }
+
+    public AsignaturaDTO(Asignatura asignatura){
+        this.id = asignatura.getId();
+        this.nombre = asignatura.getNombre();
+        this.idCurso = asignatura.getIdCurso();
+        this.idEstudios = asignatura.getIdEstudios();
+        this.nivel = asignatura.getNivel();
+    }
 }
