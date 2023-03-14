672b6f38580f7be812da76b944c4b8c08f965a53
Antelo97 <oscaran_97@hotmail.com>
2023-03-14 13:50:37 +0100

    update

---
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/ComentarioController.java
@@ -0,0 +1,99 @@
+package es.aulanosa.gestionfp.controller;
+
+import es.aulanosa.gestionfp.dto.ComentarioDTO;
+import es.aulanosa.gestionfp.model.Comentario;
+import es.aulanosa.gestionfp.service.ComentarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/comentario")
+public class ComentarioController {
+    @Autowired
+    private ComentarioService service;
+
+    // Crea un nuevo comentario
+    @PostMapping("")
+    public ResponseEntity<?> create(@RequestBody ComentarioDTO comentarioDTO) {
+        Optional<Comentario> comentarioConsultado = service.findById(comentarioDTO.getId());
+
+        if (!comentarioConsultado.isPresent() && comentarioDTO.getTexto().length() <= 500) {
+            Comentario comentarioGuardado = comentarioDTO.toModel();
+            service.save(comentarioGuardado);
+            return ResponseEntity.status(HttpStatus.CREATED).body(comentarioGuardado);
+        } else if (comentarioDTO.getTexto().length() > 500) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Longitud excedida en el campo texto");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El comentario ya fue añadido previamente");
+        }
+    }
+
+    // Devuelve el comentario cuyo id coincide con el introducido
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getComentarioById(@PathVariable Integer id) {
+        Optional<Comentario> comentarioConsultado = service.findById(id);
+
+        if (comentarioConsultado.isPresent()) {
+            return ResponseEntity.ok(comentarioConsultado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ningún comentario con ese ID");
+        }
+    }
+
+    // Actualiza un comentario ya existente
+    @PutMapping("")
+    public ResponseEntity<?> update(@RequestBody ComentarioDTO comentarioDTO) {
+        Optional<Comentario> comentarioConsultado = service.findById(comentarioDTO.getId());
+
+        if (comentarioConsultado.isPresent() && comentarioDTO.getTexto().length() <= 500) {
+            Comentario comentarioActualizado = comentarioDTO.toModel();
+            service.save(comentarioActualizado);
+            return ResponseEntity.status(HttpStatus.CREATED).body(comentarioActualizado);
+        } else if (comentarioDTO.getTexto().length() <= 500) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Longitud excedida en el campo texto");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El comentario que desea modificar no existe");
+        }
+    }
+
+    // Borra el comentario cuyo id coincide con el introducido
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Integer id) {
+        Optional<Comentario> comentarioConsultado = service.findById(id);
+
+        if (comentarioConsultado.isPresent()) {
+            service.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    // Devuelve un listado con todos los comentarios
+    @GetMapping("")
+    public ResponseEntity<?> getAll() {
+        List<Comentario> comentarios = service.findAll();
+
+        if (!comentarios.isEmpty()) {
+            return ResponseEntity.ok(comentarios);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontraron comentarios");
+        }
+    }
+
+    // Devuelve un listado con todos los comentarios cuyo sistema y referencia coinciden con los introducidos
+    @GetMapping("/sistema/{sistema}/referencia/{referencia}")
+    public ResponseEntity<?> getAll(@PathVariable char sistema, int referencia) {
+        List<Comentario> comentarios = service.findAll();
+
+        if (!comentarios.isEmpty()) {
+            return ResponseEntity.ok(comentarios);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontraron comentarios");
+        }
+    }
+}
--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/ComentarioDTO.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/ComentarioDTO.java
@@ -1,5 +1,6 @@
 package es.aulanosa.gestionfp.dto;
 
+import es.aulanosa.gestionfp.model.Comentario;
 import es.aulanosa.gestionfp.model.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +17,26 @@ public class ComentarioDTO {
     private int referencia;
     private String texto;
     private Date fecha;
-    private Usuario usuarioComentado;
 
-    
+    public Comentario toModel() {
+        Comentario comentario = new Comentario();
+
+        comentario.setId(this.id);
+        comentario.setSistema(this.sistema);
+        comentario.setReferencia(this.referencia);
+        comentario.setTexto(this.texto);
+        comentario.setFecha(this.fecha);
+
+        return comentario;
+    }
+
+    public ComentarioDTO toDTO(Comentario comentario) {
+        this.setId(comentario.getId());
+        this.setSistema(comentario.getSistema());
+        this.setReferencia(comentario.getReferencia());
+        this.setTexto(comentario.getTexto());
+        this.setFecha(comentario.getFecha());
+
+        return this;
+    }
 }
