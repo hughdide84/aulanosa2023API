34cd3769693e1e950713d144a80b917066fc1da3
Lordgaviota <javiercalo02@gmail.com>
2023-03-14 10:13:12 +0100

    Cambios

---
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package es.aulanosa.gestionfp.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components()).info(new Info().title("GestionFP").description("API de gestión de formación profesional").contact(new Contact().name("").email("").url("")).version("1.0"));
+    }
+}
