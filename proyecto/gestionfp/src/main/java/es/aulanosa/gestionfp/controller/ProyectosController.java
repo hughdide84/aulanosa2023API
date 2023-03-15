2f8bad1601e95fc4484434a8c9795b43abda1fc3
Lordgaviota <javiercalo02@gmail.com>
2023-03-15 09:53:20 +0100

    Metodos

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/ProyectosController.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/ProyectosController.java
@@ -1,13 +1,84 @@
 package es.aulanosa.gestionfp.controller;
 
+import es.aulanosa.gestionfp.dto.ProyectosDTO;
+import es.aulanosa.gestionfp.model.Proyectos;
+import es.aulanosa.gestionfp.service.ProyectosService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/proyectos")
 public class ProyectosController {
 
+    @Autowired
+    private ProyectosService service;
 
+    @PostMapping("/alta")
+    @Operation(summary = "Alta")
+    //Guarda un nuevo proyecto
+    public ResponseEntity<?> altaProyecto(@RequestBody ProyectosDTO proyectosDTO) {
+        Proyectos proyectosGuardado = service.save(proyectosDTO.toModel());
 
+        if (proyectosGuardado != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(proyectosGuardado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El proyecto ya existe");
+        }
+    }
+    @GetMapping("/{id}")
+    @Operation(summary = "Consulta")
+    //Consulta un proyecto por su ID
+    public ResponseEntity<?> getUsuarioPorId(@PathVariable Integer id){
+        Proyectos proyectosConsultado = service.findById(id);
 
+        if (proyectosConsultado != null) {
+            return ResponseEntity.ok(proyectosConsultado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ningún proyecto con ese ID");
+        }
+    }
+    @PutMapping("")
+    @Operation(summary = "Editar")
+    //Edita un proyecto
+    public ResponseEntity<?> editar(@RequestBody ProyectosDTO proyectosDTO){
+        Proyectos proyectosConsultado = service.findById(proyectosDTO.getId());
 
+        if (proyectosConsultado != null) {
+            Proyectos proyectosActualizado = proyectosDTO.toModel();
+            service.save(proyectosActualizado);
+            return ResponseEntity.status(HttpStatus.CREATED).body(proyectosActualizado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ningún proyecto con ese ID");
+        }
+    }
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar")
+    //Elimina un proyecto
+    public ResponseEntity<?> eliminar(@PathVariable Integer id){
+        Proyectos proyectosConsultado = service.findById(id);
 
+        if(proyectosConsultado != null){
+            service.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Proyecto eliminado");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ningún proyecto con ese ID");
+        }
+    }
+    @GetMapping("")
+    @Operation(summary = "Listar todo")
+    //Lista todos los proyectos
+    public ResponseEntity<?> listarTodo(){
+        List<Proyectos> proyectos = service.findAll();
 
-
-
+        if (!proyectos.isEmpty()) {
+            return ResponseEntity.ok(proyectos);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No hay proyectos");
+        }
+    }
 }
--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/ProyectosDTO.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/dto/ProyectosDTO.java
@@ -1,5 +1,6 @@
 package es.aulanosa.gestionfp.dto;
 
+import es.aulanosa.gestionfp.model.Proyectos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProyectosDTO {
 
-    private int id;
+    private Integer id;
 
     private int idAlumno;
 
-    private String documento;
+    private char documento;
 
-    private String presentacion;
+    private char presentacion;
 
     private int notaDoc;
 
@@ -23,12 +24,30 @@ public class ProyectosDTO {
 
     private int notaFinal;
 
-    
+    public Proyectos toModel() {
+        Proyectos proyectos = new Proyectos();
 
+        proyectos.setId(this.id);
+        proyectos.setIdAlumno(this.idAlumno);
+        proyectos.setDocumento(this.documento);
+        proyectos.setPresentacion(this.presentacion);
+        proyectos.setNotaDoc(this.notaDoc);
+        proyectos.setNotaPres(this.notaPres);
+        proyectos.setNotaFinal(this.notaFinal);
 
+        return proyectos;
+    }
 
+    public ProyectosDTO toDTO(Proyectos proyectos) {
+        this.setId(proyectos.getId());
+        this.setIdAlumno(proyectos.getIdAlumno());
+        this.setDocumento(proyectos.getDocumento());
+        this.setPresentacion(proyectos.getPresentacion());
+        this.setNotaDoc(proyectos.getNotaDoc());
+        this.setNotaPres(proyectos.getNotaPres());
+        this.setNotaFinal(proyectos.getNotaFinal());
 
-
-
+        return this;
+    }
 
 }
