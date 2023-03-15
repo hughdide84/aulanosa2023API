d6130a1c97d58f197f55b8f40d72a87168a52a7f
Churino10 <davidlopezporven1@gmail.com>
2023-03-15 09:28:25 +0100

    a

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/repository/AlumnoEmpresaRepository.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/repository/AlumnoEmpresaRepository.java
@@ -1,7 +1,12 @@
 package es.aulanosa.gestionfp.repository;
 
 import es.aulanosa.gestionfp.model.AlumnoEmpresa;
+import es.aulanosa.gestionfp.model.Alumno;  
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlumnoEmpresaRepository extends JpaRepository<AlumnoEmpresa, Integer> {
+
+    List<Alumno>
 }
