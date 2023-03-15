cfc13e779a7eb89b220065c2ef2fd38eac9a6a48
SSSSSSSSS99 <ngpacin9@gmail.com>
2023-03-15 09:43:48 +0100

    Controller con metodos creados, falta uno dependiente del model AlumnoEmpresa

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/EmpresaController.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/EmpresaController.java
@@ -20,23 +20,26 @@ public class EmpresaController {
     @Autowired
     private EmpresaService empService;
 
-    //Se insertan datos a la tabla empresa
+
+
+    //Metodo para insertar datos a la tabla empresa
     @PostMapping
     public ResponseEntity<?> altaEmpresa(@RequestBody EmpresaDTO empresaDTO) {
 
         Empresa consultarEmpresa = empService.findById(empresaDTO.getId());
 
-        if (consultarEmpresa == null) {
+        if (consultarEmpresa == null && checkFieldSize(empresaDTO).equals("")) {
             Empresa empresaGuardada = empresaDTO.convertirModel();
             empService.save(empresaGuardada);
             return ResponseEntity.status(HttpStatus.CREATED).body(empresaDTO);
-        }
-
-        else {
+        } else if (!checkFieldSize(empresaDTO).equals("")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Longitud de caracteres excedida de los siguientes campos: " + checkFieldSize(empresaDTO));
+        } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Empresa ya existente");
         }
     }
 
+    //Este metodo busca un campo de la tabla empresa mediante su id
     @GetMapping("{id}")
     public ResponseEntity<?> consultarEmpresa(@RequestBody Integer id) {
         Empresa empresa = empService.findById(id);
@@ -49,34 +52,78 @@ public class EmpresaController {
         }
     }
 
+    //Este metodo busca un campo por id para modificarlo a partir del body que se le pasa.
     @PutMapping
     public ResponseEntity<?> editarEmpresa(@RequestBody EmpresaDTO empresaDTO) {
         Empresa empresaParaActualizar = empService.findById(empresaDTO.getId());
 
-        if (empresaParaActualizar != null) {
+        if (empresaParaActualizar != null && checkFieldSize(empresaDTO).equals("")) {
             Empresa empresaGuardada = empresaDTO.convertirModel();
             empService.save(empresaGuardada);
             return ResponseEntity.status(HttpStatus.CREATED).body(empresaGuardada);
-        }
-        else {
+        } else if (!checkFieldSize(empresaDTO).equals("")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Longitud de caracteres excedida de los siguientes campos: " + checkFieldSize(empresaDTO));
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se ha encontrado la empresa a modificar");
         }
     }
 
+    //Este metodo busca un campo de la tabla empresa para eliminarlo.
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> eliminarEmpresa(@RequestBody Integer id) {
+        Empresa empresa = empService.findById(id);
+
+        if (empresa != null) {
+            empService.deleteById(id);
+            if (empresa == null) {
+                return ResponseEntity.ok("Campo seleccionado borrado con exito");
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error, no se ha podido eliminar el campo seleccionado");
+            }
+        }
+
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se ha encontrado el campo seleccionado");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listarTodasEmpresas() {
+        List<Empresa> empresa = empService.findAll();
+
+        if (empresa != null) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(empresa);
+        }
+
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error, no se ha podido ejecutar esta funcion");
+        }
+    }
     
 
-    public String checkFieldSize(UsuarioDTO usuarioDTO) {
+    public String checkFieldSize(EmpresaDTO empresaDTO) {
         List<String> invalidFields = new ArrayList<>();
         String msg = "";
 
-        if (usuarioDTO.getNombre().length() > 50) {
+        if (empresaDTO.getNombre().length() > 100) {
             invalidFields.add("nombre");
-        } else if (usuarioDTO.getPassword().length() > 100) {
-            invalidFields.add("password");
-        } else if (usuarioDTO.getRol().length() > 10) {
-            invalidFields.add("rol");
-        } else if (usuarioDTO.getEmail().length() > 50) {
-            invalidFields.add("email");
+        } else if (empresaDTO.getDireccionSocial().length() > 300) {
+            invalidFields.add("direccionSocial");
+        } else if (empresaDTO.getDireccionTrabajo().length() > 300) {
+            invalidFields.add("direccionTrabajo");
+        } else if (empresaDTO.getCif().length() > 10) {
+            invalidFields.add("cif");
+        } else if (empresaDTO.getRepresentante().length() > 100) {
+            invalidFields.add("representante");
+        } else if (empresaDTO.getContacto().length() > 100) {
+            invalidFields.add("contacto");
+        } else if (empresaDTO.getTutor1().length() > 100) {
+            invalidFields.add("tutor1");
+        } else if (empresaDTO.getTutor2().length() > 100) {
+            invalidFields.add("tutor2");
+        } else if (empresaDTO.getTutor3().length() > 100) {
+            invalidFields.add("tutor3");
         } else {
 
         }
