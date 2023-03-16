423c6670734680516f20ef8879285409a24d3bc4
SSSSSSSSS99 <ngpacin9@gmail.com>
2023-03-16 12:28:54 +0100

    Avances

---
+++ proyecto/gestionfp/src/test/java/es/aulanosa/gestionfp/TestMensajeUsuarioV1.java
@@ -0,0 +1,11 @@
+package es.aulanosa.gestionfp;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.TestClassOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+@SpringBootTest
+public class TestMensajeUsuarioV1 {
+    
+}
