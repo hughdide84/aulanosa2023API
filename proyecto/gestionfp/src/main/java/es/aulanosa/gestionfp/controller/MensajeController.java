509dbcd2f6208e80ea7e70681280d77c87c03046
RikiPlatas <rikiplatas11@gmail.com>
2023-03-16 12:18:49 +0100

    seguir con el controller

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/MensajeController.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/MensajeController.java
@@ -1,16 +1,50 @@
 package es.aulanosa.gestionfp.controller;
 
+import es.aulanosa.gestionfp.dto.MensajeDTO;
+import es.aulanosa.gestionfp.dto.UsuarioDTO;
+import es.aulanosa.gestionfp.model.Mensaje;
+import es.aulanosa.gestionfp.model.Usuario;
+import es.aulanosa.gestionfp.service.MensajeService;
 import es.aulanosa.gestionfp.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/mensaje")
 public class MensajeController {
 
     @Autowired
-     service;
+    MensajeService service;
 
+    // Crea un nuevo usuario
+    @PostMapping("")
+    public ResponseEntity<?> crear(@RequestBody MensajeDTO mensajeDTO) {
+        Optional<Mensaje> mensajeConsultado = service.consultarPorIdMensaje(mensajeDTO.getId());
+
+        if(!mensajeConsultado.isPresent()){
+            Mensaje mensajeGuardado = mensajeDTO.toModel();
+            service.insertarMensaje(mensajeGuardado);
+            return ResponseEntity.status(HttpStatus.CREATED).body(mensajeGuardado);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("El usuario ya ha sido añadido");
+        }
+
+    }
+
+    // Devuelve el usuario cuyo id coincide con el introducido
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUsuarioById(@PathVariable Integer id) {
+        Optional<Mensaje> mensajeConsultado = service.consultarPorIdMensaje(id);
+
+        if (mensajeConsultado.isPresent()) {
+            return ResponseEntity.ok(mensajeConsultado);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe ningún mensaje con ese ID");
+        }
+    }
 
 }
---
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/MensajeService.java
@@ -0,0 +1,22 @@
+package es.aulanosa.gestionfp.service;
+
+import es.aulanosa.gestionfp.model.Estudios;
+import es.aulanosa.gestionfp.model.Mensaje;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MensajeService {
+
+    public Mensaje insertarMensaje(Mensaje mensaje);
+
+    public Optional<Mensaje> consultarPorIdMensaje(Integer id);
+
+    public List<Mensaje> consultarTodosMensajes();
+
+    public Mensaje modificarMensaje(Mensaje mensaje) throws NoSuchFieldException;
+
+    public void eliminarMensaje(Integer id);
+
+
+}
