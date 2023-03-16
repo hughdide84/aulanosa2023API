51e7421c17481259685bef022ba627992698ab97
RikiPlatas <rikiplatas11@gmail.com>
2023-03-16 09:47:17 +0100

    necesito el service de mensaje

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/MensajeController.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/controller/MensajeController.java
@@ -1,7 +1,16 @@
 package es.aulanosa.gestionfp.controller;
 
+import es.aulanosa.gestionfp.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mensaje")
 public class MensajeController {
 
-    
+    @Autowired
+     service;
+
 
 }
