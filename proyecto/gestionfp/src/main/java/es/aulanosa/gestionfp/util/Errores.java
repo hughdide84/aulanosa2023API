95b799f4c430b420303f3a29e8eefe22b880f400
hughdide <32156980+hughdide@users.noreply.github.com>
2023-03-20 20:41:22 +0100

    @ControllerAdvice

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/util/Errores.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/util/Errores.java
@@ -2,10 +2,9 @@ package es.aulanosa.gestionfp.util;
 
 public class Errores {
     public static final String COD_ERROR_DUPLICADO = "E0001";
-
     public static final String COD_ERROR_FALLOGENERAL = "E0002";
+    public static final String COD_ERROR_DATOS_ENTRADA = "E0003";
 
     public static final String MEN_ERROR_DUPLICADO = "Registro duplicado";
-
     public static final String MEN_ERROR_FALLOGENERAL = "Error de CRUD";
 }
