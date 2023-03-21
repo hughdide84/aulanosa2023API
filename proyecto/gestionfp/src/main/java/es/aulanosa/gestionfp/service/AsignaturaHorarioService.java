599fcd76cdb92a2d2724ac46f3dd166ec2a0cc00
Churino10 <davidlopezporven1@gmail.com>
2023-03-21 13:38:18 +0100

    en service

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/AsignaturaHorarioService.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/AsignaturaHorarioService.java
@@ -9,9 +9,27 @@ import java.util.Optional;
 
 public interface AsignaturaHorarioService {
 
-
+    /**
+     * Busca un AsignaturaHorario por su id
+     * @param id del AsignaturaHorario
+     * @return AsignaturaHorario con el id introducido
+     * @throws NoSeHaEncontradoException si no se encuentra el AsignaturaHorario
+     */
     Optional<AsignaturaHorario> buscarPorIdAsignaturaHorario(Integer id) throws NoSeHaEncontradoException;
+
+    /**
+     * Inserta un AsignaturaHorario en la base de datos
+     * @param asignaturaHorario a insertar
+     * @return AsignaturaHorario insertado
+     */
     AsignaturaHorario insertarAsignaturaHorario(AsignaturaHorario asignaturaHorario);
+
+    /**
+     * Modifica un AsignaturaHorario
+     * @param asignaturaHorario a modificar
+     * @return AsignaturaHorario modificado
+     * @throws NoSeHaEncontradoException si no se encuentra el AsignaturaHorario
+     */
     AsignaturaHorario modificarAsignaturaHorario(AsignaturaHorario asignaturaHorario) throws NoSeHaEncontradoException;
 
     List<AsignaturaHorario> buscarPorCursoAsignaturaHorario(int idAsignatura, int idCurso, int nivel) throws NoSeHaEncontradoException;
