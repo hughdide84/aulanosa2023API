6b8d71d120f7a49ddb5c956371300c9c3b4b4f36
hughdide <32156980+hughdide@users.noreply.github.com>
2023-03-21 21:06:18 +0100

    Despliegue

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/GestionfpApplication.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/GestionfpApplication.java
@@ -4,10 +4,11 @@ import es.aulanosa.gestionfp.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
-public class GestionfpApplication {
+public class GestionfpApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionfpApplication.class, args);
