5ac0326fa8213df9e6eaaf58e700a72f999f92f6
RikiPlatas <rikiplatas11@gmail.com>
2023-03-21 09:35:20 +0100

    seguir con service

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Entregable.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Entregable.java
@@ -2,6 +2,7 @@ package es.aulanosa.gestionfp.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,18 +13,22 @@ import java.sql.Timestamp;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Entregables {
+public class Entregable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    @Column(name = "tipo")
+    @NotNull(message = "El id de la asignatura no puede estar vacío")
     private char tipo;
     @Column(name = "idAsignatura")
     @NotNull(message = "El id de la asignatura no puede estar vacío")
     private int idAsignatura;
+    @Column(name = "descripcion")
+    @NotNull(message = "La descripcion no puede estar vacía")
+    @Size(max = 100, message = "La descripcion debe tener menos de 100 caracteres")
     private String descripcion;
+    @Column(name = "fecha")
     private Timestamp fecha;
-
 
 }
---
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/repository/EntregableRepository.java
@@ -0,0 +1,10 @@
+package es.aulanosa.gestionfp.repository;
+
+import es.aulanosa.gestionfp.model.Entregable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregableRepository extends JpaRepository<Entregable,Integer> {
+
+
+
+}
