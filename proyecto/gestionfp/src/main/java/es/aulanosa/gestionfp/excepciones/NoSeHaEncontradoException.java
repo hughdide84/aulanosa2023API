eb27e9713fa1cdca6179c8c75a83b196eec42b2d
MiguelSanCar <miguelsanchezcarneiro@gmail.com>
2023-03-22 13:19:32 +0100

    Finalizado los comentarios de las dos clases

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/excepciones/NoSeHaEncontradoException.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/excepciones/NoSeHaEncontradoException.java
@@ -1,7 +1,8 @@
 package es.aulanosa.gestionfp.excepciones;
 
 /**
- * Clase para la excepción
+ * Clase para la excepción en caso de que no se ecuentre algun dato, se utiliza manualmente y se especifica generalmente en la clase Service,--
+ * aunque se usa en los test o en el controller para controlal un fallo en los datos proporcionados y devolver un error acorde
  */
 public class NoSeHaEncontradoException extends Exception{
 
--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/excepciones/RestResponseEntityExceptionHandler.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/excepciones/RestResponseEntityExceptionHandler.java
@@ -15,9 +15,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Clase para controlar las validaciondes de las clases MODEL, en las que se especifica un requisito con Jakarta,--
+ * p.e: @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")--
+ * el anterior ejemplo produce una excepción controlada en uno de los métodos de esta clase, que devuelveautomáticamente al controller un body en el que especifica --
+ * el error más en concreto.
+ * Cada error es independiente y se añade a un LIST de errores, lo que genera más de uno en caso de que los haya
+ */
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * Método para controlar el error producido por las validaciones del MODEL
+     * @param constraintViolationException En este parámetro se especifica qué tipo de error controla, en el que se recoge en este caso un ConstraintViolationException
+     * @return Devuelve un body preparado para el Controller en el que se especifica el error/errores
+     */
     @ExceptionHandler(ConstraintViolationException.class)
     protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException constraintViolationException) {
         Set<ConstraintViolation<?>> violations = constraintViolationException.getConstraintViolations();
@@ -34,6 +46,12 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return new ResponseEntity<>(listaErrores, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Método para controlar fallos en las ejecuciones del Repository, usualmente se debe a la inserción erronea de datos cuando en la BD se necesitan otros--
+     * p.e: Se le inserta una clave foránea que no existe
+     * @param exception En este parámetro se especifica qué tipo de error controla, en el que se recoge en este caso un DataIntegrityViolationException
+     * @return Devuelve un body preparado para el Controller en el que se especifica el error/errores
+     */
     @ExceptionHandler(DataIntegrityViolationException.class)
     protected ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
         List<ErrorDTO> listaErrores = new ArrayList<>();
@@ -42,6 +60,11 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         return new ResponseEntity<>(listaErrores, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Método para controlar fallor en las ejecuciones no controlada anteriormente
+     * @param exception En este parámetro se especifica qué tipo de error controla, en el que se recoge en este caso un Exception
+     * @return Devuelve un body preparado para el Controller en el que se especifica el error/errores
+     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleDataIntegrityViolationException(Exception exception) {
         List<ErrorDTO> listaErrores = new ArrayList<>();
