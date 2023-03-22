59fff1484af469ae037a51f2e51b7b4683f00244
RikiPlatas <rikiplatas11@gmail.com>
2023-03-22 11:55:57 +0100

    docu empresas

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/EmpresaService.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/EmpresaService.java
@@ -6,11 +6,40 @@ import es.aulanosa.gestionfp.model.Empresa;
 import java.util.List;
 
 public interface EmpresaService {
-
+    /**
+     * Busca una empresa por su ID.
+     * @param id El ID de la empresa a buscar.
+     * @return La empresa encontrada.
+     */
     Empresa findById(int id);
+    /**
+     * Guarda una empresa.
+     * @param empresa La empresa a guardar.
+     * @return La empresa guardada.
+     */
     Empresa save(Empresa empresa);
+    /**
+     * Actualiza una empresa existente.
+     * @param empresa La empresa a actualizar.
+     * @return La empresa actualizada.
+     * @throws NoSeHaEncontradoException si la empresa no existe.
+     */
     Empresa update(Empresa empresa) throws NoSeHaEncontradoException;
+    /**
+     * Elimina una empresa por su ID.
+     * @param id El ID de la empresa a eliminar.
+     */
     void deleteById(int id);
+    /**
+     * Obtiene una lista de todas las empresas.
+     * @return Una lista de todas las empresas.
+     */
     List<Empresa> findAll();
+
+    /**
+     * Obtiene una lista de empresas filtradas por nombre.
+     * @param nombre El nombre a buscar.
+     * @return Una lista de empresas que coincidan con el nombre a buscar
+     */
     List<Empresa> findAllByNombre(String nombre);
 }
