37113346052f9a96deafd130bb90b456afc05019
RikiPlatas <rikiplatas11@gmail.com>
2023-03-22 09:53:21 +0100

    documentacion curso

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Curso.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/model/Curso.java
@@ -15,7 +15,7 @@ import lombok.ToString;
 import java.sql.Timestamp;
 import java.util.List;
 /**
- * Atributos para el objeto curso
+ * Clase java que representa la tabla Cursos en la BD
  */
 
 @Entity
@@ -49,6 +49,14 @@ public class Curso {
         @ToString.Exclude
         private List<Matricula> matriculas;
         //constructor sin id
+        /**
+         * Constructor para la clase Curso sin el id.
+         *
+         * @param nombre Nombre del curso.
+         * @param inicio Fecha de inicio del curso.
+         * @param fin Fecha de fin del curso.
+         * @param estado Estado del curso (activo o inactivo).
+         */
         public Curso(String nombre, Timestamp inicio, Timestamp fin, char estado) {
                 this.nombre = nombre;
                 this.inicio = inicio;
--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/CursoServiceImp.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/service/CursoServiceImp.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+/**
+ * Clase que implementa los metodos definidos en la interfaz CursoService
+ */
 @Service
 public class CursoServiceImp implements CursoService {
 
@@ -15,22 +19,27 @@ public class CursoServiceImp implements CursoService {
     //llamar al repositorio e implementar metodos
     @Autowired
     private CursoRepository repositorio;
-
-    //devuelve una lista de todos los cursos
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional(readOnly = true)
     public List<Curso> buscarTodo() {
         return repositorio.findAll();
     }
 
-    //devuelve el curso con el id que se le pase
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional(readOnly = true)
     public Curso buscarPorId(int id) {
         return repositorio.findById(id).orElse(null);
     }
 
-    //elimina el curso con el id que se le pase
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional
     public void eliminarCurso(int id) throws NoSeHaEncontradoException {
@@ -42,14 +51,18 @@ public class CursoServiceImp implements CursoService {
         }
 
     }
-    //inserta en la BD un objeto curso con los atributos que se le pase
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional
     public Curso insertarCurso(Curso curso) {
         return repositorio.save(curso);
     }
 
-    //modifica atributos de un objeto curso y lo sobreescribe
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional
     public Curso modificarCurso(Curso curso) throws NoSeHaEncontradoException {
@@ -60,7 +73,9 @@ public class CursoServiceImp implements CursoService {
         }
 
     }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     @Transactional(readOnly = true)
     //Metodo para consultar estados activos
