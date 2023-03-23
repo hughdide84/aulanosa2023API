8a1bf068f8b9a695929136cf254930961ed9ab07
Delegado <dani.mou.49@gmail.com>
2023-03-23 12:52:27 +0100

    Update AlumnoExternoRepository.java

--- proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/repository/AlumnoExternoRepository.java
+++ proyecto/gestionfp/src/main/java/es/aulanosa/gestionfp/repository/AlumnoExternoRepository.java
@@ -4,6 +4,9 @@ package es.aulanosa.gestionfp.repository;
 import es.aulanosa.gestionfp.model.AlumnoExterno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interfaz que extiende de JpaRepository para la entidad AlumnoExterno
+ */
 public interface AlumnoExternoRepository extends JpaRepository<AlumnoExterno, Integer> {
 
 
